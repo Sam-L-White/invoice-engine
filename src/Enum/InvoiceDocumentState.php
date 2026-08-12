ea5457dd052c72eda7faff1a34f7191222ad08b3
Sam-L-White <98746718+Sam-L-White@users.noreply.github.com>
2026-08-12 10:12:51 +0000

    add state to document index

--- src/Enum/InvoiceDocumentState.php
+++ src/Enum/InvoiceDocumentState.php
@@ -7,4 +7,11 @@ namespace App\Enum;
 enum InvoiceDocumentState: string
 {
     case Uploaded = 'uploaded';
+
+    public function label(): string
+    {
+        return match ($this) {
+            self::Uploaded => 'Uploaded',
+        };
+    }
 }
